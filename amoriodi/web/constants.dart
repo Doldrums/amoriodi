8ce7ee432548da9b6c56cdd75a79af44f61197ff
Arina <contact@amoriodi.xyz>
2024-01-08 03:27:53 +0200

    feat: update

--- amoriodi/web/constants.dart
+++ amoriodi/web/constants.dart
@@ -1,32 +1,38 @@
 class Constants {
   const Constants._();
 
+  static String header() => """  
+# Arina Kārlama
+Software Engineer & Researcher
+
+
+Hi! I'm Arina, 20 y.o., student of Innopolis University.
+Working as Research Assistant of Faculty of Computer Science and Software Engineering in
+Industrial Software Production Laboratory in R&D projects jointly with Huawei Labs and 
+Russian Science Foundation. 
+
+My research interests include aspects of software development processes, 
+task analysis, data mining and empirical methods. Rendering :)
+
+Now I’m based in Innopolis (void city). And! i have an incredible cat named Musya.
+
+## Recent projects
+
+""";
   static String content() => """  
-# A/Orina
+## Open Source Projects - where I had the honor of participating
 
-Hi! I'm Arina, 19 y.o. Ima pro&shy;gram&shy;ming buff with formal train&shy;ing
-in nothing. But i do my best doing something my whole life actually. 
-Now I’m based in Innopolis (void city). And! i have an incredible cat named Musya (look at right side).
-  
-</br>
-
-### Open Source Projects - where I had the honor of participating
-
----------
-Recent:
 * [0pdd.com](https://www.0pdd.com/) Puzzle Driven Development (XDSD methodology) Github Assistant
 * [PDD gem](https://github.com/cqfn/pdd) Command line toolkit for collecting syntax-sensitive TODO markers
 * [0capa-beta](https://0capa.ru/) Theoretically Objective Measurements (TOM) of Software Development Projects Assistant
 * [BLE Reader](https://pub.dev/packages/ble_reader) plugin for peripheral mode and stream data received via Bluetooth Low Energy (BLE) connection
 * [Planty](https://planty-263d3.web.app/#/) [Not released] Flutter app
 * [BetterRultor.](https://rultor.pro/#/) [LOL] kinda joke realization of [Rultor original](https://www.rultor.com/)
-* [Park At](https://parkat.ru/) Flutter Parking App
 
 to be continued..
 
-### Silly Experiments
+## Silly Experiments
 
----------
 Recent:
 * [Coins](https://github.com/Doldrums/coins) Antoine Example Flutter Application with Bloc & Freezed + Dio & Retrofit for API REST.
 * [Awesome ClojureDart](https://github.com/Doldrums/exapp) Example ClojureDart Flutter Application.
@@ -37,23 +43,28 @@ Recent:
 
 Older: [Github](https://github.com/Doldrums/) &mdash; all other stupid stuff
 
-### Social
+## Social
 
----------
 * [Github](https://github.com/Doldrums/)
 * [LinkedIn](https://www.linkedin.com/in/amoriodi/)
-* [Codewars](https://www.codewars.com/users/amoriodi/)
+* [Google Scholar](https://scholar.google.com/citations?user=-3GbddYAAAAJ)
 * [Pub (not all public nowadays tho)](https://pub.dev/publishers/amoriodi.xyz/packages/)
 
-### Publications
+## Publications
 
-* [Automatically Prioritizing and Assigning Tasks from Code Repositories in Puzzle Driven Development](https://ieeexplore.ieee.org/document/9796201) in 2022 IEEE/ACM 19th International Conference on Mining Software Repositories (MSR), Pittsburgh, PA, USA, 2022 pp. 722-723.
-* [Prioritizing Tasks in Software Development: a Systematic Literature Review](https://ieee-dataport.org/documents/prioritizing-tasks-software-development-systematic-literature-review) IEEE Dataport; 2022. Available from : https://dx.doi.org/10.21227/kyse-2a84
-* [State-of-the-art review of taxonomies for quality assessment of intelligent software systems](https://iisec.tbdakademi.org.tr/2022/wp-content/uploads/2022/12/IISEC2022-PROGRAM_v16_22.12.2022.pdf) in IISEC 2022 : 3rd International Informatics and Software Engineering Conference
+1 [Automatically Prioritizing and Assigning Tasks from Code Repositories in Puzzle Driven Development](https://ieeexplore.ieee.org/document/9796201) \n in 2022 IEEE/ACM 19th International Conference on Mining Software Repositories (MSR), Pittsburgh, PA, USA, 2022 pp. 722-723.
+
+2 [Prioritizing Tasks in Software Development: a Systematic Literature Review](https://ieee-dataport.org/documents/prioritizing-tasks-software-development-systematic-literature-review) \n IEEE Dataport; 2022. Available from : https://dx.doi.org/10.21227/kyse-2a84
+
+3 [State-of-the-art review of taxonomies for quality assessment of intelligent software systems](https://iisec.tbdakademi.org.tr/2022/wp-content/uploads/2022/12/IISEC2022-PROGRAM_v16_22.12.2022.pdf)  \n in IISEC 2022 : 3rd International Informatics and Software Engineering Conference
+
+4 [Taxonomy of Quality Assessment for Intelligent Software Systems: A Systematic Literature Review](https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=10320363)  \n IEEE Access 11, 130491-130507 
+
+5 [Automatically Prioritizing Tasks in Software Development](https://ieeexplore.ieee.org/abstract/document/10220100) \n IEEE 10.13039/501100003816-Huawei Technologies Company Ltd. under the Puzzle Driven Development (PDD) Project
+
+6 [In MSR 2022 Conference Proceedings](https://www.computer.org/csdl/proceedings-article/msr/2022/930300z005/1Eo5RSksMbS)  \n  Year: 2022, Pages: v-xvii
 
 ### About this page
-
----------
 
 This page powered by [Dart only] `webdev` and inspired by Filip Hráček compile-time Dart script accessible 
 [here](https://github.com/filiph/filiphnet/blob/master/tool/spanify.dart) but overwritten shortly [over there](https://github.com/Doldrums/amoriodi).
--- amoriodi/web/main.dart
+++ amoriodi/web/main.dart
@@ -1,31 +1,90 @@
 import 'dart:html';
+import 'dart:math';
 import 'package:markdown/markdown.dart';
-
 import 'constants.dart';
 
 class ItemUrlPolicy implements UriPolicy {
-  // RegExp regex = RegExp(r'(?:http://|https://)?.*');
+  RegExp regex = RegExp(r'(?:http://|https://)?.*');
 
-  @override
   bool allowsUri(String uri) {
-    return true;
+    return regex.hasMatch(uri);
   }
 }
+
 void main() async {
   final NodeValidatorBuilder htmlValidator = NodeValidatorBuilder.common()
-    ..allowElement('A', attributes: ['data-target', 'data-toggle', 'href'])
+    ..allowElement('a', attributes: ['data-target', 'data-toggle', 'href'])
     ..allowElement('button', attributes: ['data-target', 'data-toggle'])
     ..allowHtml5(uriPolicy: ItemUrlPolicy())
-    ..allowNavigation(ItemUrlPolicy());
-  querySelector('#output')!
-    ..setInnerHtml(
-        markdownToHtml(Constants.content()),
-        validator: htmlValidator)
-    ..animate([
-      {"opacity": 0},
-      {"opacity": 1}
-    ], {
-      "duration": 2000,
-      "fill": "forwards"
+    ..allowNavigation(ItemUrlPolicy())
+    ..allowImages(ItemUrlPolicy());
+
+  querySelector('#header')!.setInnerHtml(markdownToHtml(Constants.header()),
+      validator: htmlValidator);
+
+  final UListElement projectList =
+      querySelector('#projectList') as UListElement;
+  final DivElement videoContainer =
+      querySelector('#videoContainer') as DivElement;
+
+  final List<String> projects = [
+    "VTB Hack, Money Transfer Way",
+    "Workarounds regarding Digital Human as Service SDK",
+    "VTB Hack, Finance Assistant",
+    "Arc Mobile Fun Edition",
+    "MTS TrueTechHack, Tiflocomments for Kion Streaming Service",
+  ];
+
+  for (var projectName in projects) {
+    final LIElement listItem = LIElement()..text = projectName;
+
+    listItem.onMouseOver.listen((MouseEvent event) {
+      final itemPosition = min(max(listItem.getBoundingClientRect().top, 100),
+          window.innerHeight ?? 400 - 400);
+      videoContainer.style.top = '${itemPosition}px';
+      displayVideoForProject(projectName, videoContainer);
+      videoContainer.style.display = 'block';
     });
+
+    listItem.onMouseOut.listen((MouseEvent event) {
+      videoContainer.style.display = 'none';
+    });
+
+    projectList.children.add(listItem);
+  }
+
+  querySelector('#output')!.setInnerHtml(markdownToHtml(Constants.content()),
+      validator: htmlValidator);
+}
+
+void displayVideoForProject(String projectName, DivElement videoContainer) {
+  Map<String, String> projectVideos = {
+    "VTB Hack, Money Transfer":
+        "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4",
+    "Workarounds regarding Digital Human as Service SDK":
+        "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4",
+    "VTB Hack, Finance Assistant":
+        "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4",
+    "Arc Mobile Fun Edition":
+        "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4",
+    "MTS TrueTechHack, Tiflocomments for Kion Streaming Service":
+        "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4",
+  };
+
+  String videoUrl = projectVideos[projectName] ?? '';
+
+  if (videoUrl.isNotEmpty) {
+    VideoElement video = VideoElement()
+      ..src = videoUrl
+      ..autoplay = true
+      ..style.width = '100%'
+      ..style.height = '100%'
+      ..muted = true
+      ..setAttribute('playsinline', 'true');
+
+    videoContainer.children.clear();
+    videoContainer.children.add(video);
+  } else {
+    videoContainer.text = 'Video not available for this project';
+  }
 }
